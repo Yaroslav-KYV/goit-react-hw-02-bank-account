1d61fe3d8da9bed1fb16689435c969990020624d
Yaroslav <kovshun.yv@gmail.com>
2019-12-05 23:16:17 +0200

    Updates

---
+++ precache-manifest.fdf34d90e71e37239417701b0905ea3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202a705c948bf941005b2be357bf221c",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "8724e187093aab30439d",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "8015695c505bc6fedfdc",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.914dc127.chunk.css"
+  },
+  {
+    "revision": "8724e187093aab30439d",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.42ddff81.chunk.js"
+  },
+  {
+    "revision": "8015695c505bc6fedfdc",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.fc2d7879.chunk.js"
+  },
+  {
+    "revision": "59b69276754240abb686",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.611f8d42.js"
+  }
+]);
